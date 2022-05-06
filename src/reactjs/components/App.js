3f8494cead17341714291d33ae950e3ba72a71a5
Godwin Nwachukwu <nwgwin@gmail.com>
2022-05-06 17:39:11 +0100

    setup project

--- src/reactjs/components/App.js
+++ src/reactjs/components/App.js
@@ -1,8 +1,18 @@
-// import React from 'react';
+import React from 'react';
 // import { Routes, Route } from 'react-router-dom';
 
 import '../../assets/stylesheets/App.css';
 
-const App = () => {};
+const App = () => (
+  <div className="" data-testid="app-1">
+    <div className="header">
+      {/* <Navigation /> */}
+      logo
+    </div>
+
+    {/* <div className="main"> */}
+    {/* </div> */}
+  </div>
+);
 
 export default App;
